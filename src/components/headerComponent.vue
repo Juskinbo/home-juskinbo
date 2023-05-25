09f60c113b8b7b57cd883afa8532f75b1d9137d9
Juskinbo <juskinbo@outlook.com>
2023-05-26 01:57:42 +0800

    feat: complete header&main

---
+++ src/components/headerComponent.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="header">
+    <div class="name"><a href="/">Infinite progress</a></div>
+    <div class='websites'>
+      <a v-for="o in websites" :key="o.title" :href="o.url" class="website"><img :src="o.icon" class="icons"><span
+          class="titles">{{ o.title }}</span></a>
+    </div>
+  </div>
+</template>
+<script setup>
+const websites = [
+  { index: 1, title: 'Home', url: '/', icon: 'https://s2.loli.net/2023/05/26/vamwe9r4CxDb2LQ.png' },
+  { index: 2, title: 'Blog', url: 'https://blog.juskinbo.cn', icon: 'https://s2.loli.net/2023/05/26/Q5yxwOA1MspRbV9.png' },
+  { index: 3, title: 'Memos', url: 'https://memos.juskinbo.cn', icon: 'https://s2.loli.net/2023/05/26/tV5vOPBKY9jA6rG.png' },
+  { index: 4, title: 'Cloud', url: 'https://cloud.juskinbo.cn', icon: 'https://s2.loli.net/2023/05/26/R3cJXgif7dbvFA2.png' }
+]
+</script>
+<style scoped>
+@font-face {
+  font-family: 'brush-script';
+  src: url('../assets/BrushScript.otf');
+}
+
+a {
+  /* 删除下划线 */
+  text-decoration: none;
+  /* 不修改颜色 */
+  color: inherit;
+}
+
+img {
+  /* 自适应大小 */
+  width: auto;
+  height: 20px;
+}
+
+.header {
+  /* 所有元素放在同一排 */
+  text-align: center;
+  align-items: center;
+  margin: 0 auto;
+  display: flex;
+  font-size: 20px;
+  /* 占满一整行 */
+  width: 100%;
+  margin-top: 3vh;
+  /* position: relative; */
+}
+
+.name {
+  font-family: brush-script, system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  font-size: 24px;
+  /* 靠左放置 */
+  /* width: 10%; */
+  float: left;
+  padding-left: 20px;
+}
+
+
+
+.websites {
+  color: #777777;
+  /* 靠右放置 */
+  position: absolute;
+  margin-right: 0vw;
+  right: 0vw;
+  float: right;
+  /* list-style: none; */
+  /* 将所有的li放在同一列 */
+  display: flex;
+}
+
+.website {
+  padding-right: 20px;
+}
+
+@media screen and (min-width: 768px) {
+  .icons {
+    display: none;
+  }
+
+  .titles {
+    display: inline;
+  }
+}
+
+@media screen and (max-width: 768px) {
+  .icons {
+    display: block;
+  }
+
+  .titles {
+    display: none;
+  }
+}</style>
---
+++ src/components/mainComponent.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class='introduction'>
+    <h1 style="font-family: brush-script">Juskinbo</h1>
+    <p class="detail">
+      Halo! I'm Juskinbo, a front-end developer from China.
+      <br>
+      <br>
+      <br>
+      Student of <a href="https://www.hznu.edu.cn">HZNU</a>.
+      <br>
+      <br>
+      <br>
+      Skills: HTML, CSS, JavaScript, Vue, Node.js, Express, Git, MySQL, Flutter, C/C++, Java, Python, etc.
+      <br>
+      <br>
+      <br>
+      Find me on
+      <br>
+      <br>
+      <a href="https://github.com/Juskinbo">GitHub</a>, <a href="https://twitter.com/Juskinbo0326">Twitter</a> 
+      <br>
+      <br>
+      <br>
+      Or send an email.
+      <br>
+      <br>
+      <a href="mailto:juskinbo@outlook.com">Outlook</a>, <a href="mailto:gjiangyufan@gmail.com">Gmail</a>
+      
+    </p>
+  </div>
+</template>
+<script>
+
+</script>
+<style scoped>
+.introduction {
+  letter-spacing: 0.1em;
+  font-size: 16px;
+  margin: 5vh auto;
+  /* text-align: center; */
+  
+  height: 100%;
+  /* background-color: #000; */
+  /* position: absolute; */
+  /* top: 0; */
+  /* left: 0; */
+  /* z-index: 999; */
+  /* opacity: 0.5; */
+}
+
+.introduction {
+  background: linear-gradient(to right, #000000, #ffffff);
+  /* 定义渐变颜色 */
+  -webkit-background-clip: text;
+  /* 将渐变应用于文字 */
+  -webkit-text-fill-color: transparent; 
+}
+
+/* 定义动画效果 */
+.introduction {
+  animation: fadeIn 2s ease-in-out;
+  /* 定义动画名称、持续时间和缓动函数 */
+}
+
+@keyframes fadeIn {
+  0% {
+    opacity: 0;
+  }
+
+  /* 动画开始时，文字透明度为0 */
+  100% {
+    opacity: 1;
+  }
+
+  /* 动画结束时，文字透明度为1 */
+}
+
+@media screen and (min-width: 768px) {
+  .introduction{
+    width: 60%;
+  }
+}
+
+@media screen and (max-width: 768px) {.introduction{
+    width: 90%;
+  }}</style>
