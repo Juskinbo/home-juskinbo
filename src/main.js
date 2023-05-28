0ab5ee459f4ac6c68831af6f40874bd4069035ac
Juskinbo <juskinbo@outlook.com>
2023-05-28 12:56:07 +0800

    feat: add vue-router

--- src/main.js
+++ src/main.js
@@ -1,9 +1,26 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import * as ElementPlusIconsVue from '@element-plus/icons-vue'
-
-const app = createApp(App)
+import * as ElementPlusIconsVue from "@element-plus/icons-vue";
+import { createRouter, createWebHistory } from "vue-router";
+const app = createApp(App);
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-  app.component(key, component)
+  app.component(key, component);
 }
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/",
+      component: () => import("./components/mainComponent.vue"),
+    },
+    {
+      path: "/friends",
+      component: () => import("./components/friendsComponent.vue"),
+      meta: {
+        keepAlive: true,
+      }
+    },
+  ],
+});
+app.use(router);
 app.mount("#app");
